23296747b9c9156639f45799c173fd87c5ba5894
xu7777777 <44518662+xu7777777@users.noreply.github.com>
2019-12-27 10:10:22 +0800

    init repo

---
+++ src/main/java/com/xqy/cool/cool/CoolApplication.java
@@ -0,0 +1,13 @@
+package com.xqy.cool.cool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xqy/cool/cool/CoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xqy.cool.cool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

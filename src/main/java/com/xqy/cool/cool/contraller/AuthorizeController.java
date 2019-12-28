d68506f3e8cbcc42fe4d9f34a6f2e5627acaf3de
xu7777777 <44518662+xu7777777@users.noreply.github.com>
2019-12-28 20:15:10 +0800

    github

--- src/main/java/com/xqy/cool/cool/contraller/AuthorizeController.java
+++ src/main/java/com/xqy/cool/cool/contraller/AuthorizeController.java
@@ -1,4 +1,42 @@
 package com.xqy.cool.cool.contraller;
 
+import com.xqy.cool.cool.dto.AccessTokenDTO;
+import com.xqy.cool.cool.dto.GithubUser;
+import com.xqy.cool.cool.provider.GitHubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class AuthorizeController {
+
+    @Autowired
+    private GitHubProvider gitHubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSercret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSercret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = gitHubProvider.getUser(accessToken);
+        System.out.println(user.getLogin());
+        return "index";
+    }
 }
